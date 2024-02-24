affd94f605297ff1362be0dac361ad929b487954
Pratham Singhal <95610829+Prathamsinghal1@users.noreply.github.com>
2024-02-25 00:40:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { useState } from 'react'
 import News from './Components/News'
-// import About from './Components/About'
 import Navbar from './Components/Navbar'
-
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
 
 export default function App() {
   
@@ -15,7 +8,6 @@ export default function App() {
 
   return (
     <div className="">
-        <Router basename='/NewsNewer_react'>
           <Navbar/>
           <News/>
       </div>
